cc4018cfe02475efed4ec5cb66e0551cc59e11d2
Alexandretta <alxsalazar21@gmail.com>
2025-08-06 16:39:44 -0500

    se puede registrar y loguear con un nombre de usuario

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -43,7 +43,7 @@ const Register = () => {
             <label>Nombre de Usuario:</label>
             <input
               className="form-control"
-              type="email"
+              type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
